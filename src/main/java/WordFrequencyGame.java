bb7fcb201fcac371c2e96b576de8bed3a9111089
davidltk123 <davidltk123@gmail.com>
2020-11-30 17:22:29 +0800

    [WordFrequency][David Leung] refactor: extract methods from big method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -7,33 +7,46 @@ public class WordFrequencyGame {
 
     public String getResult(String sentence) {
         try {
-            //split the input string with 1 to n pieces of spaces
-            String[] words = sentence.split(SPACE_REGEX);
-
-            List<WordFrequency> wordCountList = new ArrayList<>();
-            for (String word : words) {
-                wordCountList.add(new WordFrequency(word, 1));
-            }
-
-            //get the map for the next step of sizing the same word
-            Map<String, List<WordFrequency>> wordFrequencyMap = getListMap(wordCountList);
-
-            List<WordFrequency> wordFrequencyList = new ArrayList<>();
-            for (Map.Entry<String, List<WordFrequency>> entry : wordFrequencyMap.entrySet()) {
-                wordFrequencyList.add(new WordFrequency(entry.getKey(), entry.getValue().size()));
-            }
+            List<WordFrequency> wordFrequencyList = calculateWorldFrequency(sentence);
 
             wordFrequencyList.sort((word1, word2) -> word2.getCount() - word1.getCount());
 
-            StringJoiner wordFrequencyResult = new StringJoiner(LINE_FEED);
-            for (WordFrequency wordFrequency : wordFrequencyList) {
-                String wordFrequencyLine = wordFrequency.getWord() + " " + wordFrequency.getCount();
-                wordFrequencyResult.add(wordFrequencyLine);
-            }
-            return wordFrequencyResult.toString();
+            return buildWordFrequencyResult(wordFrequencyList);
         } catch (Exception exception) {
             return "Calculate Error";
         }
+    }
+
+    private String buildWordFrequencyResult(List<WordFrequency> wordFrequencyList) {
+        StringJoiner wordFrequencyResult = new StringJoiner(LINE_FEED);
+        for (WordFrequency wordFrequency : wordFrequencyList) {
+            String wordFrequencyLine = buildWorldFrequencyLine(wordFrequency);
+            wordFrequencyResult.add(wordFrequencyLine);
+        }
+        return wordFrequencyResult.toString();
+    }
+
+    private String buildWorldFrequencyLine(WordFrequency wordFrequency) {
+        return String.format("%s %d", wordFrequency.getWord(), wordFrequency.getCount());
+    }
+
+    private List<WordFrequency> calculateWorldFrequency(String sentence) {
+        //split the input string with 1 to n pieces of spaces
+        String[] words = sentence.split(SPACE_REGEX);
+
+        List<WordFrequency> wordCountList = new ArrayList<>();
+        for (String word : words) {
+            wordCountList.add(new WordFrequency(word, 1));
+        }
+
+        //get the map for the next step of sizing the same word
+        Map<String, List<WordFrequency>> wordFrequencyMap = getListMap(wordCountList);
+
+        List<WordFrequency> wordFrequencyList = new ArrayList<>();
+        for (Map.Entry<String, List<WordFrequency>> entry : wordFrequencyMap.entrySet()) {
+            wordFrequencyList.add(new WordFrequency(entry.getKey(), entry.getValue().size()));
+        }
+        return wordFrequencyList;
     }
 
     private Map<String, List<WordFrequency>> getListMap(List<WordFrequency> wordFrequencyList) {
