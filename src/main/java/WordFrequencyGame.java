50b0a30524351dd03678d882b159cb28d3d5dadb
davidltk123 <davidltk123@gmail.com>
2020-11-30 17:18:59 +0800

    [WordFrequency][David Leung] refactor: remove redundant if-else

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -6,38 +6,33 @@ public class WordFrequencyGame {
     private static final String LINE_FEED = "\n";
 
     public String getResult(String sentence) {
+        try {
+            //split the input string with 1 to n pieces of spaces
+            String[] words = sentence.split(SPACE_REGEX);
 
-        if (sentence.split(SPACE_REGEX).length == 1) {
-            return sentence + " 1";
-        } else {
-            try {
-                //split the input string with 1 to n pieces of spaces
-                String[] words = sentence.split(SPACE_REGEX);
-
-                List<WordFrequency> wordCountList = new ArrayList<>();
-                for (String word : words) {
-                    wordCountList.add(new WordFrequency(word, 1));
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<WordFrequency>> wordFrequencyMap = getListMap(wordCountList);
-
-                List<WordFrequency> wordFrequencyList = new ArrayList<>();
-                for (Map.Entry<String, List<WordFrequency>> entry : wordFrequencyMap.entrySet()) {
-                    wordFrequencyList.add(new WordFrequency(entry.getKey(), entry.getValue().size()));
-                }
-
-                wordFrequencyList.sort((word1, word2) -> word2.getCount() - word1.getCount());
-
-                StringJoiner wordFrequencyResult = new StringJoiner(LINE_FEED);
-                for (WordFrequency wordFrequency : wordFrequencyList) {
-                    String wordFrequencyLine = wordFrequency.getWord() + " " + wordFrequency.getCount();
-                    wordFrequencyResult.add(wordFrequencyLine);
-                }
-                return wordFrequencyResult.toString();
-            } catch (Exception exception) {
-                return "Calculate Error";
+            List<WordFrequency> wordCountList = new ArrayList<>();
+            for (String word : words) {
+                wordCountList.add(new WordFrequency(word, 1));
             }
+
+            //get the map for the next step of sizing the same word
+            Map<String, List<WordFrequency>> wordFrequencyMap = getListMap(wordCountList);
+
+            List<WordFrequency> wordFrequencyList = new ArrayList<>();
+            for (Map.Entry<String, List<WordFrequency>> entry : wordFrequencyMap.entrySet()) {
+                wordFrequencyList.add(new WordFrequency(entry.getKey(), entry.getValue().size()));
+            }
+
+            wordFrequencyList.sort((word1, word2) -> word2.getCount() - word1.getCount());
+
+            StringJoiner wordFrequencyResult = new StringJoiner(LINE_FEED);
+            for (WordFrequency wordFrequency : wordFrequencyList) {
+                String wordFrequencyLine = wordFrequency.getWord() + " " + wordFrequency.getCount();
+                wordFrequencyResult.add(wordFrequencyLine);
+            }
+            return wordFrequencyResult.toString();
+        } catch (Exception exception) {
+            return "Calculate Error";
         }
     }
 
