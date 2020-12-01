5de933866f373d6143d25290d0847cd111bac510
davidltk123 <davidltk123@gmail.com>
2020-12-01 12:43:47 +0800

    [Word Frequency][David Leung] refactor: remove useless exception class

--- src/main/java/CalculateException.java
+++
@@ -1,8 +0,0 @@
-public class CalculateException extends Exception{
-
-    private static final String CALCULATE_ERROR = "Calculate Error";
-
-    public CalculateException() {
-        super(CALCULATE_ERROR);
-    }
-}
