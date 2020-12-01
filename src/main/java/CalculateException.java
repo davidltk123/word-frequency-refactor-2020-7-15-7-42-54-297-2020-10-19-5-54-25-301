9029c74bf588011b8559c094afe91943bd27e73c
davidltk123 <davidltk123@gmail.com>
2020-12-01 10:03:20 +0800

    [Word Frequency][David Leung] refactor: extract magic string for exception

--- src/main/java/CalculateException.java
+++ src/main/java/CalculateException.java
@@ -1,5 +1,8 @@
 public class CalculateException extends Exception{
+
+    private static final String CALCULATE_ERROR = "Calculate Error";
+
     public CalculateException() {
-        super("Calculate Error");
+        super(CALCULATE_ERROR);
     }
 }
